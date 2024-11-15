aa1aff95560960fc5a48efcb5418f64cadd95470
stolarchukboris <kotkotovic07@gmail.com>
2024-11-16 00:53:00 +0300

    add message logger

---
+++ events/messageDelete.js
@@ -0,0 +1,17 @@
+const { Events, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: Events.MessageDelete,
+    execute (message) {
+        const embed = new EmbedBuilder()
+            .setColor(0x2B2D31)
+            .setTitle(`Message deleted from ${message.channel.url}`)
+            .setDescription(`**Created on <t:${Math.round(message.createdTimestamp / 1000)}:F> by <@${message.author.id}>**:\n${message.content}`)
+            .setTimestamp()
+            .setFooter({ text: `Spy Moderation Module`});
+
+        const channel = message.guild.channels.cache.find(channel => channel.name === 'server-logs');
+
+        channel.send({ embeds: [embed] });
+    },
+};
