cb41d1d31b111d11b26c0f65140e2db492be1e28
stolarchukboris <kotkotovic07@gmail.com>
2024-11-24 20:57:48 +0300

    add react command

---
+++ commands/react.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const ownerId = process.env.OWNER_ID;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('react')
+		.setDescription('React to a certain message.')
+        .addStringOption(option =>
+            option
+                .setName('message_id')
+                .setDescription('ID of the message you want to react to.')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('reaction')
+                .setDescription('The reaction itself.')
+                .setRequired(true)
+        ),
+	async execute(interaction) {
+		if (interaction.user.id === ownerId) {
+            const id = interaction.options.getString('message_id', true);
+            const reaction = interaction.options.getString('reaction', true);
+
+            const message = interaction.channel.messages.cache.get(id);
+            
+            message.react(reaction);
+            await interaction.reply({ content: 'Reacted!', ephemeral: true })
+        } else {
+            const mbed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setTitle(`Error.`)
+                .setDescription(`You are not authorized to run this command.`)
+                .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Noto_Emoji_Oreo_2757.svg/1200px-Noto_Emoji_Oreo_2757.svg.png')
+                .setTimestamp()
+                .setFooter({ text: 'Spy' });
+
+            await interaction.reply({ embeds: [mbed] });
+        }
+	},
+};
