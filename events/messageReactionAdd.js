774f5c1c9a8bc447c00a8b40fd8ef629b91440fa
stolarchukboris <kotkotovic07@gmail.com>
2024-11-28 22:14:19 +0300

    add utility bot database, implement starboard message auto update (closes #2)

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,10 +1,18 @@
 const { Events, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const channelId = process.env.STARS_CH_ID;
+const mysql = require('@mysql/xdevapi');
+
+const db_config = {
+	password: process.env.DB_PASS,
+    user: process.env.DB_USER,
+    host: 'localhost',
+    port: 33060,
+    schema: process.env.DB_SCHEMA
+}
 
 module.exports = {
 	name: Events.MessageReactionAdd,
 	async execute(messageReaction) {
-		
 		if (messageReaction.emoji.name === '⭐' && messageReaction.count >= 1 && messageReaction.message.channel.name !== 'stars') {
 			const channel = messageReaction.client.channels.cache.get(channelId);
 			let desc;
@@ -15,31 +23,58 @@ module.exports = {
 				desc = '';
 			}
 
-            const embed = new EmbedBuilder()
-				.setColor(0xFFAC33)
-				.setAuthor({ name: messageReaction.message.author.username, iconURL: messageReaction.message.author.avatarURL() })
-				.setDescription(`[**Jump to message**](${messageReaction.message.url})\n\n${desc}`)
-				.setTimestamp()
-				.setFooter({ text: 'Spy' })
+			mysql.getSession(db_config)
+				.then(async session => {
+					try {
+						const embed = new EmbedBuilder()
+							.setColor(0xFFAC33)
+							.setAuthor({ name: messageReaction.message.author.username, iconURL: messageReaction.message.author.avatarURL() })
+							.setDescription(`[**Jump to message**](${messageReaction.message.url})\n\n${desc}`)
+							.setTimestamp()
+							.setFooter({ text: 'Spy' })
 
-			let files = [];
+						let files = [];
 
-			try {
-				messageReaction.message.attachments.each(
-					attachment => {
-						const file = new AttachmentBuilder(attachment.url);
-						files.push(file);
-					}
-				);
-			} catch (error) {
-				console.error(error);
-			}
+						try {
+							messageReaction.message.attachments.each(
+								attachment => {
+									const file = new AttachmentBuilder(attachment.url);
+									files.push(file);
+								}
+							);
+						} catch (error) {
+							console.error(error);
+						}
 
-			channel.send({ content: `${messageReaction.message.channel.url} | ${messageReaction.count} :star:`, embeds: [embed] });
+						await session.sql(`select starboardMessage from starboardMessages where originMessage = ${messageReaction.message.id};`).execute()
+							.then(async result => {
+								const msgId = result.fetchOne();
 
-			if (files.length !== 0) {
-				channel.send({ files: files });
-			}
+								if (msgId === undefined) {
+									msg = await channel.send({ content: `${messageReaction.message.channel.url} | ${messageReaction.count} :star:`, embeds: [embed] });
+
+									if (files.length !== 0) {
+										channel.send({ files: files });
+									}
+
+									await session.sql(`insert into starboardMessages values (${messageReaction.message.id}, ${await msg.id}, ${messageReaction.count});`).execute();
+								} else {
+									await session.sql(`update starboardMessages set amountOfReactions = ${messageReaction.count} where originMessage = ${messageReaction.message.id};`).execute();
+									await session.sql(`select starboardMessage from starboardMessages where originMessage = ${messageReaction.message.id};`).execute()
+										.then(async result => {
+											const msgId = result.fetchOne()[0];
+											const msgToEdit = channel.messages.cache.get(msgId);
+
+											await msgToEdit.edit(`${messageReaction.message.channel.url} | ${messageReaction.count} :star:`);
+										});
+								}
+							});
+					} catch (error) {
+						console.error(error);
+					} finally {
+						return await session.close();
+					};
+				})
         }
 	},
 };
