b2ffea5e2f7732eda89086991fcd1dd1079ea551
stolarchukboris <kotkotovic07@gmail.com>
2024-12-01 00:28:15 +0300

    de-clarify interaction errors

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -24,11 +24,11 @@ module.exports = {
 		} catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
-				embed.setDescription(`An error has occured while executing this command.\n${error}`);
+				embed.setDescription(`An error has occured while executing this command.\nCheck logs for more information.`);
 
 				await interaction.followUp({ embeds: [embed], ephemeral: true });
 			} else {
-				embed.setDescription(`An error has occured while executing this command.\n${error}`);
+				embed.setDescription(`An error has occured while executing this command.\nCheck logs for more information.`);
 				
 				await interaction.reply({ embeds: [embed], ephemeral: true });
 			}
