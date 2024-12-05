befcd658af589ff9a68b3e7d9466e15ae078e49a
stolarchukboris <kotkotovic07@gmail.com>
2024-12-05 13:26:59 +0300

    add error handling

--- commands/utility/settings.js
+++ commands/utility/settings.js
@@ -9,10 +9,10 @@ const db_config = {
 }
 
 module.exports = {
-	category: 'utility',
-	data: new SlashCommandBuilder()
-		.setName('settings')
-		.setDescription('Manage bot settings on this server.')
+    category: 'utility',
+    data: new SlashCommandBuilder()
+        .setName('settings')
+        .setDescription('Manage bot settings on this server.')
         .addSubcommand(subcommand =>
             subcommand
                 .setName('alter')
@@ -26,17 +26,24 @@ module.exports = {
                         )
                         .setRequired(true)
                 )
-                .addIntegerOption(option =>
+                .addStringOption(option =>
                     option
                         .setName('value')
                         .setDescription('Set a value for the setting.')
                         .setRequired(true)
                 )
         ),
-	async execute(interaction) {
+    async execute(interaction) {
         await interaction.deferReply();
 
-		const session = mysql.getSession(db_config)
+        const errorEmbed = new EmbedBuilder()
+            .setColor(0xFF0000)
+            .setTitle('Error.')
+            .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Noto_Emoji_Oreo_2757.svg/1200px-Noto_Emoji_Oreo_2757.svg.png')
+            .setTimestamp()
+            .setFooter({ text: 'Spy' });
+
+        const session = mysql.getSession(db_config)
             .then(async session => {
                 if (interaction.options.getSubcommand() === 'alter') {
                     const setting = interaction.options.getString('setting', true);
@@ -51,10 +58,27 @@ module.exports = {
                                 .setThumbnail('https://septik-komffort.ru/wp-content/uploads/2020/11/galochka_zel.png')
                                 .setTimestamp()
                                 .setFooter({ text: 'Spy Configuration' });
-                                
+
                             await interaction.followUp({ embeds: [embed] });
                         })
+                        .catch(async error => {
+                            console.error(error);
+
+                            errorEmbed.setDescription(`${error}`);
+
+                            await interaction.followUp({ embeds: [errorEmbed] });
+                            return await session.close();
+                        });
                 }
+                return await session.close();
             })
-	},
+            .catch(async error => {
+                console.error(error);
+
+                errorEmbed.setDescription(`${error}`);
+
+                await interaction.followUp({ embeds: [errorEmbed] });
+                return await session.close();
+            });
+    },
 };
