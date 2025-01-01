3109f7de3300291e4335d5303c5e6e4556e0cf3e
stolarchukboris <kotkotovic07@gmail.com>
2025-01-02 00:34:50 +0300

    add deploy and pull commands

---
+++ commands/utility/pull.js
@@ -0,0 +1,68 @@
+const { spawn } = require('child_process');
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pull')
+        .setDescription('[DEV] Pull latest changes from Git Main and restart.'),
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        if (interaction.user.id === process.env.OWNER_ID) {
+            await interaction.editReply('`Pulling...`')
+
+            const { stdout, stderr } = spawn('git pull', { shell: true });
+
+            stderr.on('data', async (data) => {
+                console.log(`stderr: ${data}`);
+
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Red)
+                            .setTitle('stderr')
+                            .setDescription('There are some warnings or information that console gave')
+                            .setFields([
+                                { name: 'Output', value: `\`\`\`\n${data}\`\`\`` },
+                            ])
+                            .setTimestamp()
+                            .setFooter({ text: interaction.guild.name })
+                    ]
+                });
+            });
+
+            stdout.on('data', async (data) => {
+                console.log(`stdout: ${data}`);
+
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Blurple)
+                            .setTitle('Pull Operation')
+                            .setDescription('The command is being executed, watch output for results.')
+                            .setFields([
+                                { name: 'Output', value: `\`\`\`\n${data}\`\`\`` },
+                            ])
+                            .setTimestamp()
+                            .setFooter({ text: interaction.guild.name })
+                    ]
+                });
+
+                if (data !== 'Already up to date.') {
+                    await new Promise(resolve => setTimeout(resolve, 30_000));
+                    return process.exit();
+                };
+            });
+        } else {
+            const mbed = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setTitle(`Error.`)
+                .setDescription(`You are not authorized to run this command.`)
+                .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Noto_Emoji_Oreo_2757.svg/1200px-Noto_Emoji_Oreo_2757.svg.png')
+                .setTimestamp()
+                .setFooter({ text: 'Spy' });
+
+            return await interaction.editReply({ embeds: [mbed] });
+        };
+    }
+};
