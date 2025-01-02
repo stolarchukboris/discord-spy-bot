9e3004c74514507e81daaf91bcc38d5613229c61
stolarchukboris <kotkotovic07@gmail.com>
2025-01-02 19:07:25 +0300

    version control, /about command revamp, bot control revamp

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -1,36 +1,64 @@
-const { SlashCommandBuilder, EmbedBuilder, codeBlock } = require('discord.js');
-const os = require("os");
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
+const os = require('os');
 const packageJSON = require("../../package.json");
+const moment = require('moment');
+const child_process = require('child_process');
+
+function formatDuration(duration) {
+    return moment.duration(duration).format("d[ Days]・h[ Hrs]・m[ Mins]・s[ Secs]");
+};
 
 module.exports = {
     category: 'utility',
     data: new SlashCommandBuilder()
         .setName('about')
-        .setDescription('Get information on the bot and bot host.'),
+        .setDescription('Get information about this bot.'),
     async execute(interaction) {
-        let str = ``;
-        for (const [k, v] of Object.entries(packageJSON.dependencies)) {
-            str = str.concat(`${k}: ${v}\n`);
-        }
+        await interaction.deferReply();
 
-        const uptime = Math.floor(process.uptime());
-        const date = new Date(0);
+        let gitHash;
 
-        date.setSeconds(uptime);
-        const uptimeStr = date.toISOString().substring(11, 19);
+        try {
+            gitHash = child_process
+                .execSync('git rev-parse HEAD')
+                .toString()
+                .trim();
+        } catch (error) {
+            gitHash = 'Unknown.';
+        };
 
-        const mbed = new EmbedBuilder()
-            .setColor(0x00FF00)
-            .setTitle(`Spy`)
-            .setDescription(`Spy bot is brought to you by <@${process.env.OWNER_ID}>. You can visit the bot's GitHub repository [here](https://github.com/PleasedontslammykeyboarddfsdoijfwR/discord-spy-bot).`)
-            .addFields(
-                { name: 'Host System', value: codeBlock('yaml', `OS: ${os.machine()} ${os.platform()}-${os.release()}\nCPU: ${os.arch()} ${os.cpus()[0].model}\nUsed RAM: ${Math.round((os.totalmem() - os.freemem()) / 1048576)} MB\nUptime: ${new Date(os.uptime() * 1000).toISOString().substring(11, 19)}`) },
-                { name: 'Bot', value: codeBlock('yaml', `Used RAM: ${Math.round(process.memoryUsage().heapUsed / 1048576)} MB\nUptime: ${uptimeStr}`), inline: true },
-                { name: 'Dependencies', value: codeBlock('yaml', `${str}`), inline: true }
-            )
-            .setTimestamp()
-            .setFooter({ text: 'Spy' });
-
-        return await interaction.reply({ embeds: [mbed] });
-    },
+        return await interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(Colors.Green)
+                    .setTitle('Spy Information')
+                    .setDescription('Spy statistics and information. The bot repository can be found [here](https://github.com/PleasedontslammykeyboarddfsdoijfwR/discord-spy-bot).')
+                    .setFields([
+                        {
+                            name: "General Stats",
+                            value: `\`\`\`yml\nName: ${interaction.client.user.username}#${interaction.client.user.discriminator}\nID: ${interaction.client.user.id}\`\`\``,
+                            inline: true
+                        },
+                        {
+                            name: "Bot Stats",
+                            value: `\`\`\`yml\nGuilds: ${interaction.client.guilds.cache.size}\nUptime: ${formatDuration(interaction.client.uptime)}\nNodeJS: ${process.version}\nMemory Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\`\`\``,
+                            inline: true
+                        },
+                        {
+                            name: "System Stats",
+                            value: `\`\`\`yml\nOS: ${os.platform() + " " + os.release()}\nArch: ${os.machine()}\nUptime: ${formatDuration(os.uptime() * 1000)}\n\`\`\``
+                        },
+                        {
+                            name: "Build Stats",
+                            value: `\`\`\`yml\nSpy: v${packageJSON.version}\nBuild: ${gitHash}\n\`\`\``,
+                        },
+                    ])
+                    .setTimestamp()
+                    .setFooter({
+                        text: interaction.guild.name,
+                        iconURL: interaction.guild.iconURL()
+                    })
+            ]
+        });
+    }
 };
