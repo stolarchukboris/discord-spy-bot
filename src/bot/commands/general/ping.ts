8ded12ffdccfeaa6993b31734506952dc7850fbf
stolarchukboris <kotkotovic07@gmail.com>
2025-05-18 22:23:20 +0300

    nevermind

--- src/bot/commands/general/ping.ts
+++ src/bot/commands/general/ping.ts
@@ -1,7 +1,6 @@
 import { botCommand } from "../../../types/global.js";
 import { spyBot } from "../../../index.js";
-import { ChatInputCommandInteraction, Colors, MessageFlags } from 'discord.js';
-import builder from 'djs-cv2-embed-builder';
+import { ChatInputCommandInteraction, EmbedBuilder, Colors } from 'discord.js';
 
 export default class pingCommand implements botCommand {
     name: Lowercase<string> = "ping";
@@ -14,17 +13,6 @@ export default class pingCommand implements botCommand {
 
     async execute(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
         await interaction.editReply({
-            flags: MessageFlags.IsComponentsV2,
-            components: [
-                new builder()
-                    .setColor(Colors.Green)
-                    .setTitle('Ping.')
-                    .setDescription(`Websocket heartbeat: ${interaction.client.ws.ping} ms.`)
-                    .setThumbnail(this.spyBot.logos.heart)
-                    .setFooter('Spy Bot')
-            ]
-        });
-        /*await interaction.editReply({
             embeds: [
                 new EmbedBuilder()
                     .setColor(Colors.Fuchsia)
@@ -34,6 +22,6 @@ export default class pingCommand implements botCommand {
                     .setTimestamp()
                     .setFooter({ text: 'Spy' })
             ]
-        });*/
+        });
     }
 }
