7f4dee0a675632aa58faebf8af58e69b7257699c
stolarchukboris <kotkotovic07@gmail.com>
2025-05-23 19:38:30 +0300

    testing my "cdn"

--- src/bot/commands/general/canvas.ts
+++ src/bot/commands/general/canvas.ts
@@ -1,6 +1,6 @@
 import { botCommand } from "../../../types/global.js";
 import { spyBot } from "../../../index.js";
-import { ChatInputCommandInteraction, AttachmentBuilder, EmbedBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
 import Canvas from '@napi-rs/canvas';
 
 export default class canvasCommand implements botCommand {
@@ -23,18 +23,15 @@ export default class canvasCommand implements botCommand {
         // This uses the canvas dimensions to stretch the image onto the entire canvas
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
         context.drawImage(avatar, 20, 20, 200, 200);
-
-        // Use the helpful Attachment class structure to process the file for you
-        const thing = new AttachmentBuilder(await canvas.encode('png'), { name: 'yourimage.png' });
         
-        await interaction.editReply({
+        const message = await interaction.editReply({
             embeds: [
                 new EmbedBuilder()
                     .setTitle('You.')
                     .setDescription('This is you')
-                    .setImage('attachment://yourimage.png')
-            ],
-            files: [thing]
+                    .setImage(`http://localhost:3000/cdn/joking.png`)
+            ]
         });
+        console.log(message.embeds[0].image);
     }
 }
